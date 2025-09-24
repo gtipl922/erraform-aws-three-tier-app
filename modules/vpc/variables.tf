d2ef0c8580df25d7ca46815329192c14f9c38261
Nawaf Mohammed Musthafa <96732099+Nawaf1208@users.noreply.github.com>
2025-09-24 12:32:30 +0530

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,35 @@
+# modules/vpc/variables.tf
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_app_subnet_cidrs" {
+  description = "List of private app subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "private_db_subnet_cidrs" {
+  description = "List of private database subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
